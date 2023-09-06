62bb245c9f2b2f7f73d032e79e9a1a4c49164c5f
casey <cburhoe1@jh.edu>
2023-09-06 18:51:39 -0400

    fix invalid variable names

--- csf_assign01/uint256.c
+++ csf_assign01/uint256.c
@@ -10,9 +10,9 @@
 UInt256 uint256_create_from_u32(uint32_t val) {
   UInt256 result;
   for (int i = 0; i < 8; ++i) {
-      num.data[i] = 0U;
+      val.data[i] = 0U;
   }
-  num.data[0] = val;
+  val.data[0] = val;
   return result;
 }
 
