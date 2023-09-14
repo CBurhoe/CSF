7e299dbc6a271e2624073f79fab9270e1e4c6243
casey <cburhoe1@jh.edu>
2023-09-14 18:10:13 -0400

    Fix errors - Refactor format as hex

--- csf_assign01/uint256.c
+++ csf_assign01/uint256.c
@@ -63,18 +63,36 @@ char *uint32_to_hex_string(uint32_t value) {
 // Return a dynamically-allocated string of hex digits representing the
 // given UInt256 value.
 char *uint256_format_as_hex(UInt256 val) {
-  char *hex = (char *)malloc(65 * sizeof(char)); //num_words * hex_chars_per_word + \0
-  if (hex == NULL) {
-    fprintf(stderr, "Memory allocation failed.\n");
-    exit(1);
-  }
+  char *hex = (char *)calloc(65 * sizeof(char)); //num_words * hex_chars_per_word + \0
+  uint32_t leadingZeroes = 1;
   hex[0] = '\0'; // Initialize the hex string with a null terminator
   
   for (int i = 7; i >= 0; --i) {
-    char *hexWord = uint32_to_hex_string(val.data[i]);
-    strcat(hex, hexWord);
-    free(hexWord);
+    if (i == 7) {
+      if (val.data[i] == 0U) { continue;}
+      char hexWord[9];
+      sprintf(hex, "%x", val.data[i]);
+      strcat(hex, hexWord);
+      leadingZeroes = 0;
+    }
+  }else{
+    if (val.data[i] == 0U && leadingZeroes) {continue;}
+    if (leadingZeroes) {
+      char hexWord[9];
+      sprintf(hex, "%x", val.data[i]);
+      strcat(hex, hexWord);
+      leadingZeroes = 0;
+    }else{
+      char hexWord[9];
+      sprintf(hex, "%08x", val.data[i]);
+      hexWord[8] = '\0';
+      strcat(hex, hexWord);
+    }
   }
+  if (!strlen(hex)) {
+    strcat(hex, "0");
+  }
+  
   return hex;
 }
 
