d27ba2382e0bcb2843e8f5096ea8292615a1d9fa
casey <cburhoe1@jh.edu>
2023-09-24 11:55:38 -0400

    implement wc_free_chain

--- csf_assign02/c_wcfuncs.c
+++ csf_assign02/c_wcfuncs.c
@@ -202,5 +202,8 @@ struct WordEntry *wc_dict_find_or_insert(struct WordEntry *buckets[], unsigned n
 
 // Free all of the nodes in given linked list of WordEntry objects.
 void wc_free_chain(struct WordEntry *p) {
-  // TODO: implement
+  if (p->next != NULL) {
+    wc_free_chain(p->next);
+  }
+  free(p);
 }
