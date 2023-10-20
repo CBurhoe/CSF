e4151c4ad599e5d013a9d30f7386121f5a7a9531
casey <cburhoe1@jh.edu>
2023-10-20 14:15:03 -0400

    include cstdint

--- csf_assign03/csim.cpp
+++ csf_assign03/csim.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <stdlib.h>
 #include "cache_structure.h"
 
 int main(int argc, char **argv) {
   //TODO: Implement
+  //Check for correct number of arguments
+  if (argc != 7) {
+    std::cerr << "Incorrect number of arguments" << std::endl;
+    return 1;
+  }
+  
+  int num_sets = strtol(argv[1], nullptr, 10);
+  int num_blocks = strtol(argv[2], nullptr, 10);
+  int block_size = strtol(argv[3], nullptr, 10);
+  std::string write_allocate = argv[4];
+  std::string write_policy = argv[5];
+  std::string eviction_policy = argv[6];
+  
+  
   return 0;
 }
