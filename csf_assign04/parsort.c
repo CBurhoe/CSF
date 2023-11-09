ae57289bd4d5cbf38686eb20afe3130fe89813d7
casey <cburhoe1@jh.edu>
2023-11-08 20:50:58 -0500

    implement merge_sort

--- csf_assign04/parsort.c
+++ csf_assign04/parsort.c
@@ -113,7 +113,7 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
     exit(9);
   }
   
-  int64_t *temparr;
+  int64_t temparr[end - begin];
   merge(arr, begin, mid, end, temparr);
   for (int i = 0; i < end - begin; i++) {
     arr[i] = temparr[i];
