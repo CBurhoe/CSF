ba292fce7cc555f2c226dcac919361f88cec2561
casey <cburhoe1@jh.edu>
2023-11-28 12:14:39 -0500

    Delete unnecessary server initialization

--- csf_assign05/receiver.cpp
+++ csf_assign05/receiver.cpp
@@ -19,22 +19,44 @@ int main(int argc, char **argv) {
   std::string room_name = argv[4];
 
   Connection conn;
-
-  // TODO: connect to server
+  Message server_response;
+  
+  // DONE: connect to server
   //connect via hostname and port number
   conn.connect(server_hostname, server_port);
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
   Message r_login = Message(TAG_RLOGIN, username); //fixme: initializing structs?
-  conn.send(r_login);
+  if (!conn.send(r_login)) {
+    //TODO: handle failed send
+  }
   //TODO: listen for response
+  if (!conn.receive(server_response)) {
+    //TODO: handle failed receive
+  }
+  if (!server_response.tag == TAG_OK) {
+    //TODO: handle failed r_login
+  }
   Message join = Message(TAG_JOIN, room_name);
-  conn.send(join);
+  if (!conn.send(join)) {
+    //TODO: handle failed send
+  }
   //TODO: listen for response
-  
+  if (!conn.receive(server_response)) {
+    //TODO: handle failed receive
+  }
+  if (!server_response.tag == TAG_OK) {
+    //TODO: handle failed join
+  }
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
-
+  while(true) {
+    Message new_message;
+    if (!conn.receive(new_message)) {
+      //TODO: handle failed receive
+    }
+    
+  }
 
   return 0;
 }
