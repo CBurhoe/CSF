04e091c3a158e95afd6516f5623e94f2108b6dd4
casey <cburhoe1@jh.edu>
2023-11-30 23:10:13 -0500

    fix bug receiver:79

--- csf_assign05/receiver.cpp
+++ csf_assign05/receiver.cpp
@@ -76,7 +76,7 @@ int main(int argc, char **argv) {
     } else if (new_message.tag == TAG_DELIVERY) {
       //DONE: handle delivered message from user
       //usage: delivery:room:sender:message_text
-      std::vector delivery_payload = new_message.split_delivery_payload();
+      std::vector<std::string> delivery_payload = new_message.split_delivery_payload();
       std::string sender = delivery_payload[1];
       std::string message_text = delivery_payload[2];
       std::cout << sender << ": " << message_text << endl; //extra newline?
