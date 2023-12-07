ef61f44ce1e47566c48dfb5ee1bea8634e01bba9
casey <cburhoe1@jh.edu>
2023-12-07 10:07:15 -0500

    Begin implementing worker

--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -47,9 +47,12 @@ void *worker(void *arg) {
   // TODO: use a static cast to convert arg from a void* to
   //       whatever pointer type describes the object(s) needed
   //       to communicate with a client (sender or receiver)
+  struct ClientInfo *info = arg;
 
   // TODO: read login message (should be tagged either with
   //       TAG_SLOGIN or TAG_RLOGIN), send response
+  //buffer to store message line from fd
+  char login_message[Message::MAX_LEN + 1];
 
   // TODO: depending on whether the client logged in as a sender or
   //       receiver, communicate with the client (implementing
@@ -78,16 +81,16 @@ Server::~Server() {
 }
 
 bool Server::listen() {
-  // TODO: use open_listenfd to create the server socket, return true
+  // DONE: use open_listenfd to create the server socket, return true
   //       if successful, false if not
   std::string s = std::to_string(port);
   const char* pn = s.c_str();
   this->m_ssock = Open_listenfd(pn);
-  return (this->m_ssock >= 0);
+  return (this->m_ssock > 0);
 }
 
 void Server::handle_client_requests() {
-  // TODO: infinite loop calling accept or Accept, starting a new
+  // DONE: infinite loop calling accept or Accept, starting a new
   //       pthread for each connected client
   while(1) {
     int c_fd = Accept(this->m_ssock, NULL, NULL);
@@ -96,17 +99,19 @@ void Server::handle_client_requests() {
       exit(1);
     }
     
+    //FIXME: find end of scope of info and free()
     struct ClientInfo *info = malloc(sifeof(struct ClientInfo));
     info->client_fd = c_fd;
     
     pthread_t thread_id;
     
+    //FIXME: need to find end of thread scope and destroy zombie?
     Pthread_create(&thread_id, NULL, worker, info);
   }
 }
 
 Room *Server::find_or_create_room(const std::string &room_name) {
-  // TODO: return a pointer to the unique Room object representing
+  // DONE: return a pointer to the unique Room object representing
   //       the named chat room, creating a new one if necessary
   auto search_rooms = this->m_rooms.find(room_name);
   if (search_rooms != this->m_rooms.end()) {
