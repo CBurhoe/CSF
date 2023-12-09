76a2de0dd479d4e66d352d95ca0892b827db8b73
casey <cburhoe1@jh.edu>
2023-12-09 17:06:00 -0500

    Change size of critical section

--- csf_assign05/room.cpp
+++ csf_assign05/room.cpp
@@ -18,7 +18,7 @@ Room::~Room() {
 void Room::add_member(User *user) {
   // TODO: add User to the room
   {
-    Guard g(lock);
+    Guard(this->lock);
     this->members.insert(user);
   }
 }
@@ -26,7 +26,7 @@ void Room::add_member(User *user) {
 void Room::remove_member(User *user) {
   // TODO: remove User from the room
   {
-    Guard g(lock);
+    Guard(this->lock);
     auto it = members.find(user);
     if (it != members.end()) {
       members.erase(it);
@@ -37,10 +37,10 @@ void Room::remove_member(User *user) {
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
   // TODO: send a message to every (receiver) User in the room
   struct Message new_message = new Message();
+  new_message.tag = TAG_DELIVERY;
+  new_message.data = this->room_name + ":" + sender_username + ":" + message_text;
   { //lock the room using a guard during the critical section
-    Guard g(lock);
-    new_message.tag = TAG_DELIVERY;
-    new_message.data = this->room_name + ":" + sender_username + ":" + message_text;
+    Guard(this->lock);
     for (User *usr: this->members) {
       if (usr->is_receiver) {
         usr->mqueue.enqueue(new_message);
